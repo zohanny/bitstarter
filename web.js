2fa67983a853e4eddc4150f1a1367b03cecd6ea6
zohanny <queiros.joao@gmail.com>
2013-07-09 22:00:42 +0000

    Updated web.js 22

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 2 changed!');
 });
 
 var port = process.env.PORT || 5000;
